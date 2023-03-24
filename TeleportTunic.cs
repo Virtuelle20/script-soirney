43d53027fadee46aa275e4990625b6f37a28ef29
Virtuelle20 <128737917+Virtuelle20@users.noreply.github.com>
2023-03-24 20:44:29 +0100

    Add files via upload

---
+++ TeleportTunic.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class TeleportTunic : MonoBehaviour
+{
+    public Sprite skin; // Le skin sélectionné
+    public Transform target; // La cible de téléportation
+    public float cooldown = 1f; // Temps de recharge en secondes
+    private bool canTeleport = true; // Variable qui indique si la téléportation est possible ou non
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (canTeleport && skin != null && spriteRenderer.sprite == skin)
+            {
+                transform.position = target.position;
+                canTeleport = false; // Désactiver la téléportation
+                Invoke("ActivateTeleport", cooldown); // Activer la téléportation après le temps de recharge
+            }
+        }
+    }
+
+    private void ActivateTeleport()
+    {
+        canTeleport = true; // Réactiver la téléportation
+    }
+}
